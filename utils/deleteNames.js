e26beb144163d9670b533489fb139f19fccdb298
Ross Kelly <rkelly@mrn.org>
2015-02-16 12:54:58 -0700

    make delete util clearer

--- utils/deleteNames.js
+++ utils/deleteNames.js
@@ -1,4 +1,7 @@
 'use strict';
+/* deleteNames is a command line only utility
+ * to clear the database of names
+ */
 var nameMatcher = require("./lib/nameMatcher.js");
 nameMatcher.whenReady.then(function(value) {
     nameMatcher.clearNames();
