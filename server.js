2d335ecc5b8fc4165d065ac16fd79d0274d71a9e
Ross Kelly <rkelly@mrn.org>
2017-01-23 14:57:51 -0600

    Move namematcher over to the mono
    Initial mono move, contains lint autofixes and file cleanup

--- server.js
+++ server.js
@@ -1,67 +1,68 @@
 'use strict';
-var config = require('config').get('server');
-if (config.environment === "production") {
-    process.env.NODE_ENV = 'production';
+
+const nameMatcher = require('./lib/nameMatcher.js');
+const hapi = require('hapi');
+const boom = require('boom');
+const config = require('config').get('server');
+
+if (config.environment === 'production') {
+  process.env.NODE_ENV = 'production';
 } else {
-    process.env.NODE_ENV = 'development';
+  process.env.NODE_ENV = 'development';
 }
-var nameMatcher = require("./lib/nameMatcher.js");
-var hapi = require('hapi');
-var fs = require('fs');
-var boom = require('boom');
-nameMatcher.whenReady.then(function(value) {
-    var options = {};
-    if (config.ssl.enabled) {
-        options.tls = require('./lib/sslCredentials.js');
-    }
-    options.port = 3500;
-    var server = new hapi.Server();
-    server.connection(options);
+nameMatcher.whenReady.then((value) => {
+  const options = {};
+  if (config.ssl.enabled) {
+    options.tls = require('./lib/sslCredentials.js');
+  }
+  options.port = 3500;
+  const server = new hapi.Server();
+  server.connection(options);
 
     // match names
-    server.route({
-        method: 'GET',
-        path: '/{names}',
-        handler: function (request, reply) {
-            var nameList = request.params.names.split(":");
-            var nameObjects  = nameList.map(function createNameObjects(current) {
-                var nameObj = {};
-                current = current.split(",");
-                nameObj.first = current[0];
-                nameObj.last = current[1];
-                
-                return nameObj;
-            });
-          
-            reply(nameMatcher.matchNames(nameObjects));
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/{names}',
+    handler(request, reply) {
+      const nameList = request.params.names.split(':');
+      const nameObjects = nameList.map((current) => {
+        const nameObj = {};
+        current = current.split(',');
+        nameObj.first = current[0];
+        nameObj.last = current[1];
+
+        return nameObj;
+      });
+
+      reply(nameMatcher.matchNames(nameObjects));
+    },
+  });
     // add names
-    server.route({
-        method: 'POST',
-        path: '/',
-        handler: function (request, reply) {
-            if (request.payload.first !== undefined  
-                && request.payload.last !== undefined ){
-	        var firstList = request.payload.first.split(',');
-                var lastList = request.payload.last.split(',');
-                if (firstList.length !== lastList.length) {
-                    reply(boom.badRequest('failure: unpaired first/last'));
-                }
-                for (var i=0; i < firstList.length; i++) {
-                    if (firstList[i] !== '' || lastList[i] !== '') {
-                       nameMatcher.addName(firstList[i], lastList[i]);
-                    } else {
-                        reply(boom.badRequest('failure: empty names in list'));
-                    }
-                }
-                reply('success');
-            } else {
-                reply(boom.badRequest('failure: no names'));
-            }
+  server.route({
+    method: 'POST',
+    path: '/',
+    handler(request, reply) {
+      if (request.payload.first !== undefined
+                && request.payload.last !== undefined) {
+	        const firstList = request.payload.first.split(',');
+        const lastList = request.payload.last.split(',');
+        if (firstList.length !== lastList.length) {
+          reply(boom.badRequest('failure: unpaired first/last'));
         }
-    });
-    server.start(function () {
-        console.log('Server running at:', server.info.uri);
-    });
+        for (let i = 0; i < firstList.length; i++) {
+          if (firstList[i] !== '' || lastList[i] !== '') {
+            nameMatcher.addName(firstList[i], lastList[i]);
+          } else {
+            reply(boom.badRequest('failure: empty names in list'));
+          }
+        }
+        reply('success');
+      } else {
+        reply(boom.badRequest('failure: no names'));
+      }
+    },
+  });
+  server.start(() => {
+    console.log('Server running at:', server.info.uri);
+  });
 });
